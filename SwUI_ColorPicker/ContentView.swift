e0001bed36221e2ec676d2af19b1b98b6c61fdc4
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2022-10-07 06:33:14 +0200

    Initial Commit

---
+++ SwUI_ColorPicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwUI_ColorPicker
+//
+//  Created by Miguel on 7/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwUI_ColorPicker/SwUI_ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUI_ColorPickerApp.swift
+//  SwUI_ColorPicker
+//
+//  Created by Miguel on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUI_ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
